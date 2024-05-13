a710618439e1f7c0d0e428c1e9128e41e9a27969
Vitalii Antipovich <voantipovich@edu.hse.ru>
2024-05-13 21:57:55 +0300

    Some automatically generated files

---
+++ cmake-build-debug/CMakeFiles/gamer_club.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamer_club.dir/Client.cpp.o"
+  "CMakeFiles/gamer_club.dir/Client.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/ClientsBase.cpp.o"
+  "CMakeFiles/gamer_club.dir/ClientsBase.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/Event.cpp.o"
+  "CMakeFiles/gamer_club.dir/Event.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/GamerClubEnv.cpp.o"
+  "CMakeFiles/gamer_club.dir/GamerClubEnv.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/ParserConsole.cpp.o"
+  "CMakeFiles/gamer_club.dir/ParserConsole.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/ParserConsoleException.cpp.o"
+  "CMakeFiles/gamer_club.dir/ParserConsoleException.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/ParserFile.cpp.o"
+  "CMakeFiles/gamer_club.dir/ParserFile.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/ParserFileError.cpp.o"
+  "CMakeFiles/gamer_club.dir/ParserFileError.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/Table.cpp.o"
+  "CMakeFiles/gamer_club.dir/Table.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/TableInterval.cpp.o"
+  "CMakeFiles/gamer_club.dir/TableInterval.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/TimeStamp.cpp.o"
+  "CMakeFiles/gamer_club.dir/TimeStamp.cpp.o.d"
+  "CMakeFiles/gamer_club.dir/main.cpp.o"
+  "CMakeFiles/gamer_club.dir/main.cpp.o.d"
+  "gamer_club"
+  "gamer_club.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamer_club.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
